0d544fff42fca539c2a03cb0b05be071127e64bc
Denis Bezrukov <6227442+Dominionys@users.noreply.github.com>
2022-02-12 22:18:46 +0200

    feat: add parsers and options with regexp

---
+++ crates/depcheck_core/src/options.rs
@@ -0,0 +1,98 @@
+use regex::RegexSet;
+
+pub struct CheckerOptions {
+    pub ignore_patterns: RegexSet,
+}
+
+impl Default for CheckerOptions {
+    fn default() -> Self {
+        let ignore_patterns = RegexSet::new(&[
+            r".git",
+            r".svn",
+            r".hg",
+            r".idea",
+            r"^node_modules$",
+            r"^dist$",
+            r"^build$",
+            r"^bower_components$",
+            // Images
+            r"\.png$",
+            r"\.gif$",
+            r"\.jpg$",
+            r"\.jpeg$",
+            r"\.svg$",
+            // Fonts
+            r"\.woff$",
+            r"\.woff2$",
+            r"\.eot$",
+            r"\.ttf$",
+            // Archives
+            r"\.zip$",
+            r"\.gz$",
+            // Videos
+            r"\.mp4$",
+        ])
+        .unwrap();
+        CheckerOptions { ignore_patterns }
+    }
+}
+
+mod tests {
+    use super::CheckerOptions;
+
+    #[test]
+    fn should_ignore_png() {
+        let options = CheckerOptions::default();
+
+        let is_match = options.ignore_patterns.is_match("image.png");
+        assert!(is_match);
+
+        let is_match = options.ignore_patterns.is_match("image.png.");
+        assert!(!is_match);
+
+        let is_match = options.ignore_patterns.is_match("imagepng");
+        assert!(!is_match);
+    }
+
+    #[test]
+    fn should_ignore_node_modules() {
+        let options = CheckerOptions::default();
+
+        let is_match = options.ignore_patterns.is_match("node_modules");
+        assert!(is_match);
+
+        let is_match = options.ignore_patterns.is_match("node_module");
+        assert!(!is_match);
+
+        let is_match = options.ignore_patterns.is_match("ode_modules");
+        assert!(!is_match);
+    }
+
+    #[test]
+    fn should_ignore_dist() {
+        let options = CheckerOptions::default();
+
+        let is_match = options.ignore_patterns.is_match("dist");
+        assert!(is_match);
+
+        let is_match = options.ignore_patterns.is_match("distt");
+        assert!(!is_match);
+
+        let is_match = options.ignore_patterns.is_match("ist");
+        assert!(!is_match);
+    }
+
+    #[test]
+    fn should_ignore_build() {
+        let options = CheckerOptions::default();
+
+        let is_match = options.ignore_patterns.is_match("build");
+        assert!(is_match);
+
+        let is_match = options.ignore_patterns.is_match("bbuild");
+        assert!(!is_match);
+
+        let is_match = options.ignore_patterns.is_match("uild");
+        assert!(!is_match);
+    }
+}
---
+++ crates/depcheck_core/src/parsers.rs
@@ -0,0 +1,77 @@
+use regex::Regex;
+use std::collections::HashMap;
+use std::path::Path;
+use swc_common::comments::SingleThreadedComments;
+use swc_common::errors::{ColorConfig, Handler};
+use swc_common::sync::Lrc;
+use swc_common::SourceMap;
+use swc_ecma_ast::Module;
+use swc_ecma_parser::lexer::Lexer;
+use swc_ecma_parser::{EsConfig, Parser, StringInput, Syntax, TsConfig};
+
+pub struct Parsers {
+    inner: HashMap<&'static str, Syntax>,
+}
+
+impl Default for Parsers {
+    fn default() -> Self {
+        let inner = HashMap::from([
+            (
+                r"\.tsx?$",
+                Syntax::Typescript(TsConfig {
+                    tsx: true,
+                    ..Default::default()
+                }),
+            ),
+            (
+                r"\.jsx?$",
+                Syntax::Es(EsConfig {
+                    jsx: true,
+                    ..Default::default()
+                }),
+            ),
+        ]);
+        Parsers { inner }
+    }
+}
+
+impl Parsers {
+    pub fn parse_file(&self, file: &Path) -> Option<Module> {
+        let file_name = file.file_name().unwrap().to_string_lossy();
+
+        let syntax = self
+            .inner
+            .iter()
+            .find(|value| {
+                let regex = Regex::new(value.0).unwrap();
+                regex.is_match(file_name.as_ref())
+            })?
+            .1;
+
+        let cm: Lrc<SourceMap> = Default::default();
+        let handler = Handler::with_tty_emitter(ColorConfig::Auto, true, false, Some(cm.clone()));
+
+        let fm = cm.load_file(file).expect("failed to load");
+
+        let comments = SingleThreadedComments::default();
+        let lexer = Lexer::new(
+            syntax.to_owned(),
+            Default::default(),
+            StringInput::from(&*fm),
+            Some(&comments),
+        );
+
+        let mut parser = Parser::new_from(lexer);
+
+        for error in parser.take_errors() {
+            error.into_diagnostic(&handler).emit();
+        }
+
+        let module = parser
+            .parse_module()
+            .map_err(|e| e.into_diagnostic(&handler).emit())
+            .expect("failed to parser module");
+
+        Some(module)
+    }
+}
