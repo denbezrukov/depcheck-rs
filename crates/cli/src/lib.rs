23dd1066b9448cbab8e35df5bd204814f96cfdde
Denis Bezrukov <6227442+dominionys@users.noreply.github.com>
2022-03-23 18:08:57 +0200

    chore: update clap

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,17 +1,27 @@
-use clap::{crate_version, Arg, Command};
+use std::env;
+use clap::{crate_version, crate_authors, Arg, Command};
 
 pub fn run_cli() {
     let app = Command::new("depcheck-rs")
+        .author(crate_authors!())
+        .version(crate_version!())
         .about("The dependency check CLI")
         .bin_name("depcheck-rs")
-        .version(crate_version!())
         .arg(
             Arg::new("directory")
+                .long("directory")
                 .short('d')
                 .takes_value(true)
                 .value_name("DIRECTORY")
                 .help("Provide a directory"),
+        )
+        .arg(
+            Arg::new("ignore-bin-package")
+                .long("ignore-bin-package")
+                .default_value("false"),
         );
 
-    println!("{:#?}", app.get_matches());
+    let matches = app.get_matches();
+    let a: bool = matches.value_of_t("ignore-bin-package").unwrap();
+    println!("{:#?}", a);
 }
