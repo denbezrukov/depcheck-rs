ee72ac36d026869d705b43a14b4f66028ac4e79d
Denis Bezrukov <6227442+dominionys@users.noreply.github.com>
2022-03-31 13:37:18 +0300

    feat: add CI for napi

--- __tests__/depcheck.test.ts
+++ __tests__/depcheck.test.ts
@@ -1,4 +1,4 @@
-const {depcheck} = require('../index');
+const {depcheck} = require('../index.js');
 
 test('should not failed', () => {
   const result = depcheck('./crates/core/tests/fake_modules/bad');
