b7185b322b29244a95216f56616263f24b3e58d4
Dominionys <6227442+Dominionys@users.noreply.github.com>
2022-04-07 23:02:20 +0300

    chore: inline snapshot

--- node/src/__tests__/depcheck.test.ts
+++ node/src/__tests__/depcheck.test.ts
@@ -2,5 +2,14 @@ const {depcheck} = require('../index');
 
 test('should not failed', () => {
   const result = depcheck('./crates/core/tests/fake_modules/bad');
-  expect(result).toMatchSnapshot();
+  expect(result).toMatchInlineSnapshot(`
+Object {
+  "missingDependencies": Object {},
+  "unusedDependencies": Array [
+    "optimist",
+  ],
+  "unusedDevDependencies": Array [],
+  "usingDependencies": Object {},
+}
+`);
 });
