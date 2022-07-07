31b49248fe95a6c87434048e6baf45739d88389f
Denis Bezrukov <dbezrukov@ctrader.local>
2022-07-07 16:08:58 +0300

    chore(deps): bump workspaces versions

--- crates/cli/src/args.rs
+++ crates/cli/src/args.rs
@@ -1,4 +1,5 @@
 use std::path::{Path, PathBuf};
+use clap::builder::ValueParser;
 
 /// The dependencies checker CLI arguments.
 #[derive(Debug, clap::Parser)]
@@ -11,40 +12,42 @@ pub struct Args {
     #[clap(long, short = 'd')]
     #[clap(help = "The directory argument is the root directory of your project")]
     #[clap(default_value = ".")]
-    #[clap(parse(from_os_str))]
     #[clap(takes_value = true)]
-    #[clap(allow_invalid_utf8 = true)]
-    #[clap(validator = validate_directory)]
+    #[clap(value_parser = ValueParser::os_string())]
+    #[clap(value_parser = validate_directory)]
     pub directory: PathBuf,
 
     /// A flag to indicate if depcheck ignores the packages containing bin entry.
     #[clap(long = "ignore-bin-package")]
     #[clap(help = "A flag to indicate if depcheck ignores the packages containing bin entry")]
+    #[clap(value_parser)]
     pub ignore_bin_package: bool,
 
     /// A flag to indicate if depcheck skips calculation of missing dependencies.
     #[clap(long = "skip-missing")]
     #[clap(help = "A flag to indicate if depcheck skips calculation of missing dependencies")]
+    #[clap(value_parser)]
     pub skip_missing: bool,
 
     /// Path to a file with patterns describing files to ignore.
     #[clap(long = "ignore-path")]
     #[clap(help = "Path to a file with patterns describing files to ignore")]
-    #[clap(parse(from_os_str))]
     #[clap(takes_value = true)]
-    #[clap(allow_invalid_utf8 = true)]
+    #[clap(value_parser = ValueParser::os_string())]
     pub ignore_path: Option<PathBuf>,
 
     /// Comma separated patterns describing files or directories to ignore.
     #[clap(long = "ignore-patterns")]
     #[clap(help = "Comma separated patterns describing files or directories to ignore")]
     #[clap(use_value_delimiter = true)]
+    #[clap(value_parser)]
     pub ignore_patterns: Option<Vec<String>>,
 
     /// A comma separated array containing package names to ignore.
     #[clap(long = "ignore_matches")]
     #[clap(help = "A comma separated array containing package names to ignore")]
     #[clap(use_value_delimiter = true)]
+    #[clap(value_parser)]
     pub ignore_matches: Option<Vec<String>>,
 
     /// logging level
@@ -57,10 +60,10 @@ fn is_existing_directory(path: &Path) -> bool {
 }
 
 /// validation function for directory argument
-fn validate_directory(path: &str) -> eyre::Result<()> {
+fn validate_directory(path: &str) -> eyre::Result<PathBuf> {
     let path = PathBuf::from(path);
     if is_existing_directory(&path) {
-        Ok(())
+        Ok(path)
     } else {
         Err(eyre::eyre!("directory doesn't exist."))
     }
